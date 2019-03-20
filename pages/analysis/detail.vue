5fa3ac14daebd4a18706294a5411c40a9ed5f9ef
谢泽丰 <616973936@qq.com>
2019-03-20 16:16:35 +0800

    【style】删除一些无用代码

--- pages/analysis/detail.vue
+++ pages/analysis/detail.vue
@@ -144,15 +144,10 @@
 	page,
 	view {
 		display: flex;
-		/* uni-app默认使用flex布局。因为flex布局有利于跨更多平台，尤其是采用原生渲染的平台。如不了解flex布局，请参考http://www.w3.org/TR/css3-flexbox/。若不需要flex布局可删除本行*/
 	}
 
 	page {
 		min-height: 100%;
-	}
-
-	.page-section-title {
-		padding: 0 30upx;
 	}
 
 	.title {
@@ -171,17 +166,10 @@
 		flex-direction: column;
 	}
 
-	.navigate {
-		color: #007AFF;
-	}
-
 	.canvas-bar {
 		display: flex;
 		flex-direction: row;
 		justify-content: space-between;
 	}
 
-	.update-btn {
-		margin-right: 30upx;
-	}
 </style>
