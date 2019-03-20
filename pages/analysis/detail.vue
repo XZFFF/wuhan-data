2d7760ab82a2a5d1b6607f36592dd15a52610b77
谢泽丰 <616973936@qq.com>
2019-03-20 16:11:25 +0800

    【feat】新增ECharts的demo，绘制饼状图和折线图

--- pages/analysis/detail.vue
+++ pages/analysis/detail.vue
@@ -1,16 +1,112 @@
 <template>
-	<view>
-		{{indexId}}
+	<!-- <view> -->
+	<view class="container">
+		<view class="canvasView">
+			<view class="canvas-bar">
+				<view class="title">饼图示例</view>
+			</view>
+			<mpvue-echarts :echarts="echarts" :onInit="pieInit" canvasId="pie" ref="pieChart" />
+		</view>
+
+		<view class="canvasView">
+			<view class="title">折线图示例</view>
+			<mpvue-echarts :echarts="echarts" :onInit="lineInit" canvasId="line" ref="lineChart" />
+		</view>
 	</view>
+	<!-- </view> -->
 </template>
 
 <script>
+	import * as echarts from '../../components/echarts/echarts.simple.min.js';
+	import mpvueEcharts from '../../components/mpvue-echarts/src/echarts.vue';
+	const cityList = [{
+		value: 55,
+		name: '北京'
+	}, {
+		value: 38,
+		name: '上海'
+	}, {
+		value: 20,
+		name: '广州'
+	}];
+
+	let pieOption = {
+		animation: false,
+		backgroundColor: '#F8F8F8',
+		color: ['#37A2DA', '#32C5E9', '#67E0E3', '#91F2DE', '#FFDB5C', '#FF9F7F'],
+		series: [{
+			label: {
+				normal: {
+					fontSize: 14
+				}
+			},
+			type: 'pie',
+			center: ['50%', '50%'],
+			radius: [0, '60%'],
+			data: [],
+			itemStyle: {
+				emphasis: {
+					shadowBlur: 10,
+					shadowOffsetX: 0,
+					shadowColor: 'rgba(0, 2, 2, 0.3)'
+				}
+			}
+		}]
+	};
+
+	let lineOption = {
+		animation: false,
+		color: ['#37A2DA', '#9FE6B8'],
+		grid: {
+			x: 35,
+			x2: 10,
+			y: 30,
+			y2: 25
+		},
+		calculable: false,
+		xAxis: [{
+			type: 'category',
+			data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+		}],
+		yAxis: [{
+			type: 'value',
+			splitArea: {
+				show: true
+			}
+		}],
+		series: [{
+			name: '蒸发量',
+			type: 'line',
+			data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
+		}, {
+			name: '降水量',
+			type: 'line',
+			data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
+		}]
+	};
+
 	export default {
+		components: {
+			mpvueEcharts
+		},
 		data() {
 			return {
+				echarts: echarts,
 				indexId: "1000",
 				indexName: "指标详情页"
 			};
+		},
+		onLoad: function(e) {
+			console.log(e);
+			// typeof(e.indexId) != "undefined"
+			if (JSON.stringify(e) != '{}') {
+				this.indexId = e.indexId;
+				uni.setNavigationBarTitle({
+					title: e.indexName
+				})
+			}
+			pieOption.series[0].data = cityList.slice(0);
+			console.log("indexId:" + this.indexId);
 		},
 		// 导航栏自定义按钮
 		onNavigationBarButtonTap(e) {
@@ -19,20 +115,73 @@
 				icon: "none"
 			})
 		},
-		onLoad: function(e) {
-			console.log(e);
-			// typeof(e.indexId) != "undefined"
-			if (JSON.stringify(e) != '{}') {
-				console.log(e);
-				this.indexId = e.indexId;
-				uni.setNavigationBarTitle({
-					title: e.indexName
+		methods: {
+			pieInit(canvas, width, height) {
+				let pieChart = echarts.init(canvas, null, {
+					width: width,
+					height: height
 				})
+				canvas.setChart(pieChart)
+
+				pieChart.setOption(pieOption)
+				return pieChart
+			},
+			lineInit(canvas, width, height) {
+				let lineChart = echarts.init(canvas, null, {
+					width: width,
+					height: height
+				})
+				canvas.setChart(lineChart)
+
+				lineChart.setOption(lineOption)
+				return lineChart
 			}
 		}
 	}
 </script>
 
 <style>
+	page,
+	view {
+		display: flex;
+		/* uni-app默认使用flex布局。因为flex布局有利于跨更多平台，尤其是采用原生渲染的平台。如不了解flex布局，请参考http://www.w3.org/TR/css3-flexbox/。若不需要flex布局可删除本行*/
+	}
 
+	page {
+		min-height: 100%;
+	}
+
+	.page-section-title {
+		padding: 0 30upx;
+	}
+
+	.title {
+		margin-left: 30upx;
+		color: #8f8f94;
+	}
+
+	.container {
+		padding-bottom: 30upx;
+		box-sizing: border-box;
+	}
+
+	.container,
+	.canvasView {
+		flex: 1;
+		flex-direction: column;
+	}
+
+	.navigate {
+		color: #007AFF;
+	}
+
+	.canvas-bar {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+
+	.update-btn {
+		margin-right: 30upx;
+	}
 </style>
