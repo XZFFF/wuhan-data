515f3ef660e193edeca48d9adcf908638462b035
谢泽丰 <616973936@qq.com>
2019-03-20 15:33:52 +0800

    【fix】新增导航栏自定义按钮——收藏、分享

--- pages/analysis/detail.vue
+++ pages/analysis/detail.vue
@@ -12,6 +12,13 @@
 				indexName: "指标详情页"
 			};
 		},
+		// 导航栏自定义按钮
+		onNavigationBarButtonTap(e) {
+			uni.showToast({
+				title: e.index === 0 ? "你点了分享按钮" : "你点了收藏按钮",
+				icon: "none"
+			})
+		},
 		onLoad: function(e) {
 			console.log(e);
 			// typeof(e.indexId) != "undefined"
