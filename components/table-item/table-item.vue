e441e70b7cd98980edfed06d3b076ba796313196
谢泽丰 <616973936@qq.com>
2019-03-27 17:55:43 +0800

    【feat】表格抽象成组件

---
+++ components/table-item/table-item.vue
@@ -0,0 +1,66 @@
+<template>
+	<view class="page">
+		<scroll-view scroll-x="true" scroll-y="true" class="table">
+			<view v-for="(cloumnItem,cloumnIndex) in tableBody" :key="cloumnIndex" class="columnItem" :class="cloumnIndex == 0 ? 'columnFirst' : ''">
+				<view v-for="(rowItem,rowIndex) in cloumnItem" :key="rowIndex" class="rowItem" :class="rowIndex == 0 ? 'rowFirst' : ''">{{rowItem}}</view>
+			</view>
+		</scroll-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			tableBody: {
+				type: Array,
+				default: function(e) {
+					return [
+						['表头1', ''],
+						['表头2', '']
+					]
+				}
+			}
+		},
+		data() {
+			return {};
+		}
+	}
+</script>
+
+<style>
+	.page {
+		width: 100%;
+		overflow: hidden;
+		white-space: nowrap;
+	}
+
+	.table {
+		width: 100%;
+		overflow: hidden;
+		max-height: 700upx;
+	}
+
+	.columnItem {
+		display: inline-block;
+		text-align: center;
+		border-right: 2rpx solid #EBEEF4;
+		box-sizing: border-box
+	}
+
+	.columnFirst {
+		border-left: 2rpx solid #EBEEF4;
+	}
+
+	.rowItem {
+		border-bottom: 2rpx solid #EBEEF4;
+		padding: 15upx;
+	}
+
+	/* 表头样式 */
+	.rowFirst {
+		border-top: 2rpx solid #EBEEF4;
+		padding-top: 25upx;
+		padding-bottom: 25upx;
+		background-color: #F5F7FA;
+	}
+</style>
--- pages/analysis/demo.vue
+++ pages/analysis/demo.vue
@@ -1,25 +1,33 @@
 <template>
-	<view class="page">
+	<!-- <view class="page">
 		<scroll-view scroll-x="true" scroll-y="true" class="table">
 			<view v-for="(cloumnItem,cloumnIndex) in tableBody" :key="cloumnIndex" class="columnItem" :class="cloumnIndex == 0 ? 'columnFirst' : ''">
 				<view v-for="(rowItem,rowIndex) in cloumnItem" :key="rowIndex" class="rowItem" :class="rowIndex == 0 ? 'rowFirst' : ''">{{rowItem}}</view>
 			</view>
 		</scroll-view>
+	</view> -->
+	<view style="width: 500upx;">
+		<table-item :tableBody="tableBody"></table-item>
 	</view>
 </template>
 
 <script>
+	import tableItem from '../../components/table-item/table-item.vue';
 	export default {
+		components: {
+			tableItem
+		},
 		data() {
 			let tableBody = [
-				['表头1表头1表头头1表头1', 'item1', 'item2', 'item3', 'item4'],
-				['表头2表头1表头1表', 'xiaoming', 'xiaohong', 'lining', '233'],
-				['表头3', '9000000000', '80', '70', '233'],
-				['表头3', '900', '80', '70', '233'],
-				['表头3', '900', '80', '70', '233'],
-				['表头3', '900', '80', '70', '233']
+				['表头1表头1表头头1表头1', 'item1', 'item2', 'item3', 'item4', '233'],
+				['表头2表头1表头1表', 'xiaoming', 'xiaohong', 'lining', '233', '233'],
+				['表头3', '9000000000', '80', '70', '233', '233'],
+				['表头3', '900', '80', '70', '233', '233'],
+				['表头3', '900', '80', '70', '233', '233'],
+				['表头3', '900', '80', '70', '233', '233']
 			];
 			return {
+				maxHeight: "300upx",
 				tableBody: tableBody,
 			}
 		},
@@ -29,39 +37,4 @@
 </script>
 
 <style>
-	.page {
-		width: 100%;
-		overflow: hidden;
-		white-space: nowrap;
-	}
-
-	.table {
-		width: 100%;
-		overflow: hidden;
-		max-height: 500upx;
-	}
-
-	.columnItem {
-		display: inline-block;
-		text-align: center;
-		border-right: 2rpx solid #EBEEF4;
-		box-sizing: border-box
-	}
-
-	.columnFirst {
-		border-left: 2rpx solid #EBEEF4;
-	}
-
-	.rowItem {
-		border-bottom: 2rpx solid #EBEEF4;
-		padding: 15upx;
-	}
-
-	/* 表头样式 */
-	.rowFirst {
-		border-top: 2rpx solid #EBEEF4;
-		padding-top: 25upx;
-		padding-bottom: 25upx;
-		background-color: #F5F7FA;
-	}
 </style>
