2d02313a235573490ba38ba900717e37efd74391
谢泽丰 <616973936@qq.com>
2019-04-02 10:22:03 +0800

    【fix】测试多折线图的展示

--- pages/analysis/detail.vue
+++ pages/analysis/detail.vue
@@ -6,7 +6,7 @@
 		<table-item :tableBody="tableBody"></table-item>
 		<nb-echarts :nbOption="demoOption" :canvasId="'demo'" nbHeight="300"></nb-echarts>
 		<!-- <nb-echarts :nbOption="pieOption" :canvasId="'pie'"></nb-echarts> -->
-		<!-- <nb-echarts :nbOption="lineOption" :canvasId="'line'"></nb-echarts> -->
+		<nb-echarts :nbOption="linesOption" :canvasId="'line'"></nb-echarts>
 		<nb-echarts :nbOption="nbOption" :canvasId="'nb'" nbHeight="400"></nb-echarts>
 
 	</scroll-view>
@@ -21,9 +21,7 @@
 
 	let demoOption = {
 		grid: {
-			left: "16%",
-			right: "10%",
-			top: 30
+			containLabel: true
 		},
 		xAxis: {
 			type: 'category',
@@ -37,7 +35,6 @@
 			type: 'line'
 		}]
 	};
-
 
 	let pieOption = {
 		animation: false,
@@ -117,9 +114,7 @@
 			show: false
 		},
 		grid: {
-			left: "18%",
-			right: "22%",
-			// top: "20%"
+			containLabel: true
 		},
 		legend: {
 			top: "bottom",
@@ -172,6 +167,59 @@
 		]
 	};
 
+	let linesOption = {
+		tooltip: {
+			trigger: 'axis'
+		},
+		legend: {
+			top: "bottom",
+			data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+		},
+		grid: {
+			containLabel: true
+		},
+		xAxis: {
+			type: 'category',
+			boundaryGap: false,
+			data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日', '周一', '周二', '周三', '周四', '周五']
+		},
+		yAxis: {
+			type: 'value'
+		},
+		series: [{
+				name: '邮件营销',
+				type: 'line',
+				stack: '总量',
+				data: [120, 132, 101, 134, 90, 230, 210, 120, 132, 101, 134, 213]
+			},
+			{
+				name: '联盟广告',
+				type: 'line',
+				stack: '总量',
+				data: [220, 182, 191, 234, 290, 330, 310, 220, 182, 191, 234, 290]
+			},
+			{
+				name: '视频广告',
+				type: 'line',
+				stack: '总量',
+				data: [150, 232, 201, 154, 190, 330, 410, 232, 201, 154, 190, 223]
+			},
+			{
+				name: '直接访问',
+				type: 'line',
+				stack: '总量',
+				data: [320, 332, 301, 334, 390, 330, 320, 332, 301, 334, 390, 422]
+			},
+			{
+				name: '搜索引擎',
+				type: 'line',
+				stack: '总量',
+				data: [820, 932, 901, 934, 1290, 1330, 1320, 932, 901, 934, 1290, 1321]
+			}
+		]
+	};
+
+
 
 	export default {
 		components: {
@@ -195,6 +243,7 @@
 				lineOption: lineOption,
 				pieOption: pieOption,
 				nbOption: nbOption,
+				linesOption: linesOption,
 				tableBody: tableBody,
 				indexId: "1000",
 				indexName: "指标详情页",
@@ -213,6 +262,11 @@
 		},
 		// 导航栏自定义按钮
 		onNavigationBarButtonTap(e) {
+			if (e.index === 0) {
+				uni.share({
+
+				})
+			}
 			uni.showToast({
 				title: e.index === 0 ? "你点了分享按钮" : "你点了收藏按钮",
 				icon: "none"
