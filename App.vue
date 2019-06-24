ac08f55cf1d3d3ea432e2fcde00b4260d18c7c23
谢泽丰 <616973936@qq.com>
2019-06-24 16:40:32 +0800

    【feat】进入APP直接跳转到指定界面（用于分享）

--- App.vue
+++ App.vue
@@ -1,12 +1,36 @@
 <script>
 	export default {
-		onLaunch: function () {
+		onLaunch: function() {
 			console.log('App Launch')
 		},
-		onShow: function () {
-			console.log('App Show')
+		onShow: function() {
+			console.log('App Show');
+			var args = plus.runtime.arguments;
+			if (args) {
+				// console.log(args);
+				// let argsJson = JSON.parse(args);
+				// let argsJson = {
+				// 	"name": "analysis-detail",
+				// 	"pathName": "pages/analysis/detail/detail",
+				// 	"query": "indexId=1001&indexName=GDP"
+				// };
+				// let navigateUrl = "/" + argsJson.pathName + "?" + argsJson.query;
+				// console.log(navigateUrl);
+				// // 处理args参数，如直达到某新页面等
+				// setTimeout(function() {
+				// 	uni.reLaunch({
+				// 		url: navigateUrl,
+				// 		success(res) {
+				// 			console.log('success jump');
+				// 		},
+				// 		fail(e) {
+				// 			console.log('fail jump');
+				// 		}
+				// 	});
+				// }, 1000);
+			}
 		},
-		onHide: function () {
+		onHide: function() {
 			console.log('App Hide')
 		}
 	}
@@ -15,6 +39,7 @@
 <style>
 	/*每个页面公共css */
 	@import './common/uni.css';
+
 	page {
 		background-color: #F8F8F8;
 		height: 100%;
