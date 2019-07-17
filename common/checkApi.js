c885be2a76278c464358e99c80457a5fe1ffd7e6
谢泽丰 <616973936@qq.com>
2019-07-17 16:17:45 +0800

    【feat】新增公共方法-checkNetwork/checkToken

--- common/checkApi.js
+++ common/checkApi.js
@@ -33,5 +33,36 @@ module.exports = {
 	isNumber: function(checkVal) {
 		var reg = /^-?[1-9][0-9]?.?[0-9]*$/;
 		return reg.test(checkVal);
+	},
+	checkNetwork: function() {
+		uni.getNetworkType({
+			success: function(res) {
+				if (res.networkType == 'none') {
+					uni.showToast({
+						title: '无网络连接',
+						duration: 1000,
+						icon: 'loading'
+					});
+					return false;
+				} else {
+					return true;
+				}
+			}
+		});
+		return false;
+	},
+	checkToken: function() {
+		// 检查token令牌是否存在
+		let token = uni.getStorageSync('token');
+		if (!token) {
+			// 提示登录状态过期
+			uni.showToast({
+				icon: 'none',
+				title: "登录已过期",
+				duration: 500
+			});
+			return false;
+		}
+		return true;
 	}
 }
