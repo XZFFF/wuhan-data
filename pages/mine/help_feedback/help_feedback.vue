45a5977535c24f843259a92c4a66877ba6ceaf93
谢泽丰 <616973936@qq.com>
2019-07-18 13:44:19 +0800

    【fix】完善help页缓存逻辑及模拟数据

--- pages/mine/help_feedback/help_feedback.vue
+++ pages/mine/help_feedback/help_feedback.vue
@@ -1,47 +1,74 @@
 <template>
 	<view>
 		<view class="uni-list">
-			<view class="list-cell" hover-class="uni-list-cell-hover" v-for="(value,key) in menu_list" :key="key" @click="goDetailPage(value)">
+			<view class="list-cell" hover-class="uni-list-cell-hover" v-for="(item,key) in menu_list" :key="key" @click="goHelpDetail(item.id)">
 				<view class="uni-list-cell-navigate uni-navigate-right uni-media-list">
 					<view class="uni-media-list-body" style="justify-content: center">
-						<view class="uni-media-list-text" style="font-size: 35upx">{{value.title}}</view>
+						<view class="uni-media-list-text" style="font-size: 35upx">{{item.title}}</view>
 					</view>
 				</view>
 			</view>
 		</view>
-		<input type="button" class="feedback-submit" style="line-height:80upx" value="反馈我的问题" v-for="(value,key) in feedback" :key="key" @click="goDetailPage(value)" />
+		<input type="button" class="feedback-submit" style="line-height:80upx" value="反馈我的问题" @click="goFeedback()" />
 	</view>
 </template>
 
 <script>
+	import checkApi from '@/common/checkApi.js';
+	import helpFeedbackApiJson from "@/common/api/helpFeedback.json";
+
 	export default {
-		data(){
+		data() {
 			return {
-				menu_list: [
-					{
-					title: "如何绑定/换绑手机号？",
-				},{
-					title: "如何获得职位认证？",
-				},{
-					title: "数据下载失败？",
-				}
-				],
-				
-				feedback: [
-					{
-					url: "feedback",
-				}
-				]
+				menu_list: [],
 			}
 		},
+		onShow: function() {
+			this.initHelp();
+		},
 		methods: {
-			goDetailPage(e) {
-				let path = e.url ? e.url : e;
-				let url = ~path.indexOf('platform') ? path : '../../mine/' + path + '/' + path;
-				uni.navigateTo({
-					url: url
+			initHelp() {
+				checkApi.checkNetwork();
+				uni.request({
+					url: 'http://www.baidu.com',
+					method: 'GET',
+					data: {},
+					success: (res) => {
+						try {
+							let dataApi = helpFeedbackApiJson;
+							checkApi.isApi(dataApi);
+							this.menu_list = dataApi.data.help;
+							uni.setStorageSync('help', this.menu_list);
+						} catch (e) {
+							console.log(e.errMsg);
+							this.getHelpStorage();
+						}
+					},
+					fail: (e) => {
+						console.log(e.errMsg);
+						this.getHelpStorage();
+					},
 				});
-				return false;
+			},
+			getHelpStorage() {
+				try {
+					let helpInfo = uni.getStorageSync('help');
+					if (helpInfo) {
+						this.menu_list = helpInfo;
+					}
+				} catch (e) {
+					console.log(e.message);
+				}
+			},
+			goHelpDetail(id) {
+				uni.navigateTo({
+					url: "help?id=" + id
+				});
+			},
+			goFeedback() {
+				uni.navigateTo({
+					url: "../feedback/feedback"
+				});
 			}
 		}
 	}
@@ -49,12 +76,14 @@
 
 <style>
 	page {
-	    background-color: #EFEFF4;
+		background-color: #EFEFF4;
 	}
-	.list-cell{
-		border-bottom: 1upx solid rgb(229,229,229);
+
+	.list-cell {
+		border-bottom: 1upx solid rgb(229, 229, 229);
 	}
-	.feedback-submit{
+
+	.feedback-submit {
 		margin-top: 600upx;
 		margin-left: 5%;
 		width: 90%;
@@ -62,7 +91,6 @@
 		font-size: 35upx;
 		color: black;
 		background-color: #FFFFFF;
-		border-radius: 5px; 
+		border-radius: 5px;
 	}
-	
 </style>
