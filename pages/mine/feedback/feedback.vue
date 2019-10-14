95dba9265a24698117782c546d86d17f799c686a
方应骞 <343947020@qq.com>
2019-10-14 16:26:38 +0800

    【fix】修复意见反馈页面光标未与文字对齐的问题

--- pages/mine/feedback/feedback.vue
+++ pages/mine/feedback/feedback.vue
@@ -4,8 +4,8 @@
 			<text>问题和意见</text>
 		</view>
 		<view class="feedback-body">
-			<textarea placeholder="请详细描述您的问题和意见" v-model="sendDate.text" class="feedback-textare" />
-			</view>
+			<textarea style="line-height: 0px;" placeholder="请详细描述您的问题和意见" v-model="sendDate.text" class="feedback-textare" />
+		</view>
         <view class='feedback-title'>
             <text>图片(选填,提供问题截图,总大小10M以下)</text>
         </view>
